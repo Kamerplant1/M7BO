3274143e032f85ee871fbb60e50180e5e5dc434c
Tristan Wals <39040@ma-web.nl>
2026-02-13 15:04:52 +0100

    les1

---
+++ src/AboutMe.jsx
@@ -0,0 +1,13 @@
+import './AboutMe.css';
+
+export function AboutMe(){
+    return(
+        <div className="AboutMe__container">
+        <div className="AboutMe">
+            <h1>About Me</h1>
+            <img className="AboutMe__img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNb5nbP7WCYxHGuwLFl8f-P9luge5jOjs2Sw&s" alt="image" />
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis quae repellat exercitationem excepturi corporis quaerat dignissimos hic, officia similique distinctio iste neque debitis aliquid fuga dolore dolorem nesciunt voluptatum tempore.</p>
+        </div>
+        </div>
+    )
+}
---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { AboutMe } from './AboutMe'
+
+function App() {
+
+  return (
+    <>
+    <main>
+      <AboutMe/>
+    </main>
+    </>
+  )
+}
+
+export default App
